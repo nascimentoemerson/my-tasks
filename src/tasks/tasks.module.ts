5a249f480d83b57ca26cfc717bf7409c79b7d291
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-20 21:35:15 -0300

    feat: create services

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ControllersController } from './controllers/controllers.controller';
+import { ServicesService } from './services/services.service';
 
 @Module({
-  controllers: [ControllersController]
+  controllers: [ControllersController],
+  providers: [ServicesService],
 })
 export class TasksModule {}
