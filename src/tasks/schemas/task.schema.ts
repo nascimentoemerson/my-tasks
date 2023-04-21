a9e1565f8297c6b30a3e8d3345ed4af609c9cbb9
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-21 17:15:15 -0300

    feat: refactory task.schema

--- src/tasks/schemas/task.schema.ts
+++ src/tasks/schemas/task.schema.ts
@@ -1,6 +1,13 @@
-import mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const TaskSchema = new mongoose.Schema({
-  description: String,
-  completed: Boolean,
-});
+@Schema()
+export class Task extends Document {
+  @Prop()
+  description: string;
+
+  @Prop({ default: false })
+  completed: boolean;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
