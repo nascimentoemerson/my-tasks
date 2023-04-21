24a991ca501260e2aa6fff64f5788f30eb662f1a
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-21 19:32:29 -0300

    feat: create jwt payload

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import { JwtPayload } from './jwt-payload.interface';
-import { UsersService } from '../users/users.service';
+import { JwtPayload } from 'src/interfaces/jwt-payload.interface';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
