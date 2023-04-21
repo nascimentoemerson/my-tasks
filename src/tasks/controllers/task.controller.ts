77c8ae769b84d7fc74b96d73cbe04853507edfeb
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-20 22:49:28 -0300

    feat: create crud

--- src/tasks/controllers/task.controller.ts
+++ src/tasks/controllers/task.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TaskService } from '../services/task.service';
+import { Task } from '../services/task';
 
-@Controller('task.controller')
-export class TaskController {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return this.taskService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<Task> {
+    return this.taskService.getById(id);
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+    task.id = id;
+    return this.taskService.update(task);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    this.taskService.delete(id);
+  }
+}
