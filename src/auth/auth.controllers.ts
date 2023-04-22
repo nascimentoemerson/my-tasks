ae3005dbfec400ca5bd8dece26be0f48f7d54eb3
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-22 19:36:54 -0300

    fix: auth tags

--- src/auth/auth.controllers.ts
+++ src/auth/auth.controllers.ts
@@ -1,14 +1,14 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './shared/auth.service';
 import { LocalAuthGuard } from './shared/local-auth.guard';
 import { Controller, UseGuards, Request, Post } from '@nestjs/common';
 
 @ApiTags('Auth')
 @Controller()
+@UseGuards(LocalAuthGuard)
 export class AuthController {
   constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
+  @ApiSecurity('basic')
   @Post('auth/login')
   async login(@Request() req: any) {
     return this.authService.login(req.user);
