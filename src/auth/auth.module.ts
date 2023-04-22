3769c2ad38378ff2a5277b14b124f95eda6fd4aa
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-22 19:44:11 -0300

    fix: swagger

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { jwtConstants } from './shared/constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '300s' },
     }),
   ],
   controllers: [AuthController],
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Documentação com Swagger - My Task')
-    .setDescription(
-      'documentação desenvolvida para o projeto final do curso de programador full stack',
-    )
+    .setTitle('My Tasks API')
+    .setDescription('API for managing tasks')
     .setVersion('1.0')
-    .addTag('users')
+    .addTag('tasks')
+    .addBearerAuth()
+    .addBasicAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      securityDefinitions: {
+        basicAuth: {
+          type: 'basic',
+          description: 'Basic authentication',
+        },
+      },
+      security: [
+        {
+          basicAuth: [],
+        },
+      ],
+    },
+  });
 
   await app.listen(3000);
 }
