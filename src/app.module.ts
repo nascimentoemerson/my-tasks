452430ea72c3627ca61b6b8f7678194ed3a037fa
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-22 16:12:50 -0300

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { TaskModule } from './tasks/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://emerson776475:teste@cluster0.74yrmuc.mongodb.net/?retryWrites=true&w=majority',
     ),
+    UsersModule,
     TaskModule,
     AuthModule,
   ],
